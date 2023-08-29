f2a3d03bb0e108a8f0f1998dbe421961f4d8a39c
João Queiroz <joao.pires.queiroz@gmail.com>
2023-08-29 20:42:59 +0100

    Correct country names

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -75,9 +75,9 @@ function App() {
   const [gender, _] = useState<string>(() => {
     const random = Math.random();
 
-    if (random < 0.47) {
+    if (random < 0.475) {
       return "Male";
-    } else if (random < 0.94) {
+    } else if (random < 0.95) {
       return "Female";
     } else if (random < 0.99) {
       return ["Trans Man", "Trans Woman"][Math.floor(Math.random() * 2)];
@@ -88,10 +88,15 @@ function App() {
   const [country, setCountry] = useState<string>();
   const [rate, setRate] = useState<string>();
   const [rarity, setRarity] = useState<string>();
+  const [wealth, setWealth] = useState<string>();
+  const [wealthPerc, setWealthPerc] = useState<string>();
 
   useEffect(() => {
     getRandomCountry().then(({ country, rate, rarity }) => {
-      getRandomWealth(country).then(console.log);
+      getRandomWealth(country).then(({ wealth, perc }) => {
+        setWealth("$" + wealth.toFixed(1));
+        setWealthPerc((perc * 100).toPrecision(3) + "%");
+      });
       setCountry(country);
       setRarity(rarity);
       setRate((rate * 100).toPrecision(2) + "%");
@@ -107,7 +112,10 @@ function App() {
           You had a {rate} chance (Rarity: {rarity})
         </div>
         <div className="min-h-[24px] mt-2">
-          Gender: <span className="font-bold">{gender}</span>
+          <span className="font-bold">Wealth:</span> {wealth} ({wealthPerc})
+        </div>
+        <div className="min-h-[24px] mt-2">
+          <span className="font-bold">Gender:</span> {gender}
         </div>
       </div>
     </div>
