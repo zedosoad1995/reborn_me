acffd890a2d3f61db0119e06c1baee5d24427ee5
João Queiroz <joao.pires.queiroz@gmail.com>
2023-08-29 22:22:06 +0100

    Fix build issues

--- frontend/src/Util/randomWealth.ts
+++ frontend/src/Util/randomWealth.ts
@@ -1,34 +1,6 @@
 import Papa from "papaparse";
 import quantile from "@stdlib/stats-base-dists-lognormal-quantile";
 
-async function parseCountryWealth(country: string): Promise<number[]> {
-  return new Promise((resolve, reject) => {
-    fetch("/country_wealth.csv")
-      .then((response) => response.text())
-      .then((data) => {
-        Papa.parse(data, {
-          complete: function (results) {
-            if (!Array.isArray(results.data)) {
-              return reject();
-            }
-
-            if (!Array.isArray(results.data[0])) {
-              return reject();
-            }
-
-            const row = (results.data as string[][]).find((row) => row[0] === country);
-
-            if (!row) {
-              return reject();
-            }
-
-            resolve(row.slice(3).map(Number));
-          },
-        });
-      });
-  });
-}
-
 async function parseWealthParams(country: string): Promise<number[]> {
   return new Promise((resolve, reject) => {
     fetch("/wealth_params_simple.csv")
@@ -51,28 +23,6 @@ async function parseWealthParams(country: string): Promise<number[]> {
             }
 
             resolve(row.slice(1).map(parseFloat));
-          },
-        });
-      });
-  });
-}
-
-async function parseFatTails(): Promise<number[][]> {
-  return new Promise((resolve, reject) => {
-    fetch("/usa_fat_tail.csv")
-      .then((response) => response.text())
-      .then((data) => {
-        Papa.parse(data, {
-          complete: function (results) {
-            if (!Array.isArray(results.data)) {
-              return reject();
-            }
-
-            if (!Array.isArray(results.data[0])) {
-              return reject();
-            }
-
-            resolve((results.data as string[][]).map((innerArr) => innerArr.map(parseFloat)));
           },
         });
       });
@@ -105,19 +55,6 @@ async function parseParetoRatio(country: string): Promise<number> {
         });
       });
   });
-}
-
-function lastNonZero(arr: number[]): number | null {
-  for (let i = arr.length - 1; i >= 0; i--) {
-    if (arr[i] !== 0) {
-      return arr[i];
-    }
-  }
-  return null;
-}
-
-function exp(x: number, a: number, b: number) {
-  return (Math.log(x) - b) / a;
 }
 
 function inversePareto(perc: number, a: number, b: number) {
